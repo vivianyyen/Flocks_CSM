38ce8cfd4d144f533432ae8a5ca5226b75a62541
YoongEng <sd23048@adab.umpsa.edu.my>
2026-05-12 17:13:02 +0800

    Update app.py

--- incident_dashboard/app.py
+++ incident_dashboard/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.set_page_config(
-    page_title="Incident Intelligence Dashboard",
+    page_title="Cyber Risk Intelligence: Cyber News Dashboard",
     page_icon="🛡️",
     layout="wide",
     initial_sidebar_state="expanded"
